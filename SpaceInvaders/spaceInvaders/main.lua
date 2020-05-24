3c14e8361e659eee54c1e9ab47b2fbdd8cf83b10
NewdleCat <mrfluffy900>
2020-05-24 00:12:10 -0700

    Enemies now move down

--- SpaceInvaders/spaceInvaders/main.lua
+++ SpaceInvaders/spaceInvaders/main.lua
@@ -16,15 +16,17 @@ function particle_system:spawn(x, y)
     local ps = {}
     ps.x = x
     ps.y = y
-    ps.timer = 52
+    ps.stopped = false
+    ps.timer = 45
+    ps.delete_timer = 200
     ps.ps = love.graphics.newParticleSystem(particle_system.image, 32)
     ps.ps:setParticleLifetime(2, 4)
     ps.ps:setEmissionRate(50)
     ps.ps:setSizes(0.3)
-    ps.ps:setLinearAcceleration(-10000, -10000, 10000, 10000)
+    ps.ps:setLinearAcceleration(-1000, -1000, 1000, 1000)
     ps.ps:setEmissionArea("uniform", 0, 0, 0, false)
     -- ps.ps:setRadialAcceleration(5000)
-    ps.ps:setSpeed(10)
+    ps.ps:setSpeed(0)
     -- ps:setColors(100, 255, 100, 255, 0, 255, 0, 255)
     table.insert(particle_system.list, ps)
 end
@@ -39,6 +41,10 @@ function particle_system:update(dt)
     for _, v in pairs(particle_system.list) do
         v.timer = v.timer - 1
 
+        if v.timer <= 0 then
+            v.delete_timer = v.delete_timer - 1
+        end
+
         v.ps:update(dt)
     end
 end
@@ -47,19 +53,24 @@ function particle_system:cleaner()
     for i, v in ipairs(particle_system.list) do
         if v.timer <= 0 then
             -- table.remove(particle_system.list, i)
+            -- v.ps:setSpeed(10)
             v.ps:stop()
-            v.timer = 40
+        end
+
+        if v.delete_timer <= 0 then
+            table.remove(particle_system.list, i)
         end
     end
 end
 
 function checkCollisions(enemies, bullets)
     for i, e in ipairs(enemies) do
-        for _, b in pairs(bullets) do
+        for j, b in ipairs(bullets) do
             if b.y <= e.y + e.height and b.x > e.x and b.x < e.x + e.width then
                 particle_system:spawn(e.x, e.y)
 
                 table.remove(enemies, i)
+                table.remove(bullets, j)
 
                 love.audio.setVolume(0.2)
 
@@ -78,12 +89,16 @@ function checkCollisions(enemies, bullets)
 end
 
 function love.load()
+    love.window.setMode(1024,768)
+
     game_over = false
     game_win = false
+    enemy_go_left = false
+    enemy_go_right = true
     background_image = love.graphics.newImage('background.png')
     player = {}
-    player.x = 350
-    player.y = 550
+    player.x = 500
+    player.y = 700
     player.speed = 8;
     player.bullets = {}
     player.cooldown = 0
@@ -94,14 +109,15 @@ function love.load()
             love.audio.play(player.fire_sound)
             player.cooldown = 60
             bullet = {}
-            bullet.x = player.x + 50
+            bullet.x = player.x + 21
             bullet.y = player.y
             table.insert(player.bullets, bullet)
         end
     end
 
     for i=0, 10, 1 do
-        enemies_controller:spawnEnemy(i * 70, 100)
+        enemies_controller:spawnEnemy((i * 70) + 30, 100)
+        enemies_controller:spawnEnemy((i * 70) + 30, 170)
     end
 
     -- enemies_controller:spawnEnemy(100,100)
@@ -118,7 +134,7 @@ function enemies_controller:spawnEnemy(x, y)
 
     enemy.bullets = {}
     enemy.cooldown = 20
-    enemy.speed = 0.1
+    enemy.speed = 0.2
     table.insert(self.enemies,enemy)
 end
 
@@ -159,7 +175,28 @@ function love.update(dt)
         if e.y >= love.graphics.getHeight() then
             game_over = true
         end
-        e.y = e.y + 1 * e.speed
+
+        if enemy_go_right == true then
+            e.x = e.x + 1 * e.speed
+        elseif enemy_go_left == true then
+            e.x = e.x - 1 * e.speed
+        end
+
+        if e.x + e.width >= 1014 then
+            enemy_go_right = false
+            enemy_go_left = true
+            for _,e in pairs(enemies_controller.enemies) do
+                e.y = e.y + 20
+            end
+        elseif e.x <= 10 then
+            enemy_go_right = true
+            enemy_go_left = false
+            for _,e in pairs(enemies_controller.enemies) do
+                e.y = e.y + 20
+            end
+        end
+
+
     end
 
     for i,b in ipairs(player.bullets) do
@@ -184,11 +221,14 @@ function love.draw()
 
     particle_system:draw()
 
-    -- love.graphics.print(#enemies_controller.enemies)
+    love.graphics.print(#particle_system.list)
 
     love.graphics.setColor(0, 1, 0)
-    love.graphics.rectangle("fill", player.x + 55, player.y, 2, -1000)
+    love.graphics.rectangle("fill", player.x + 25, player.y, 2, -1000)
 
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.rectangle("fill", 10, 0, 1, 1000)
+    love.graphics.rectangle("fill", 1014, 0, 1, 1000)
 
     love.graphics.setColor(1, 1, 1)
     for _,v in pairs(player.bullets) do
@@ -197,14 +237,11 @@ function love.draw()
 
     --PLAYER SPAWN
     -- love.graphics.rectangle("fill", player.x, player.y, 100, 20)
-    love.graphics.draw(player.image, player.x + 35, player.y, 0, 1)
+    love.graphics.draw(player.image, player.x, player.y, 0, 1)
 
     love.graphics.setColor(1, 1, 1)
     for _,e in pairs(enemies_controller.enemies) do -- ENEMY SPAWNER
         love.graphics.draw(enemies_controller.image, e.x, e.y, 0, 1)
-        -- love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.width, enemy.height)
     end
-
-
 
 end
